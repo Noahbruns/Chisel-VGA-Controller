2e5fa7067515701f6dde866bcd88f1365fe4a493
Noah Bruns <noah@dev.localdomain>
2020-11-18 15:12:24 +0100

    syncs implemented

--- src/main/scala/VGAController.scala
+++ src/main/scala/VGAController.scala
@@ -1,10 +1,10 @@
 package VGAController
 
 import chisel3._
+import chisel3.util._
 
 class VGAController extends Module {
   val io = IO(new Bundle {
-    //val pixel_clock = Input(UInt(1.W))
     val n_blank = Output(UInt(1.W))
     val h_sync = Output(UInt(1.W))
     val v_sync = Output(UInt(1.W))
@@ -14,24 +14,58 @@ class VGAController extends Module {
   io.h_sync := 0.U
   io.v_sync := 0.U
 
+  /* Vertical */
+  val v_front_porch = 1 // in lines
+  val v_back_porch = 23
+  val v_sync = 4
+  val v_display = 640
+
+  val frame_height = v_display + v_front_porch + v_sync + v_back_porch
+
+  val v_cntReg = RegInit(0.U(log2Ceil(frame_height).W))
+
   /* Horizontal */
-  val h_front_porch = 40.U // in pixels
-  val h_back_porch = 88.U
-  val h_sync = 128.U
-  val h_display = 800.U
+  val h_front_porch = 40
+  val h_back_porch = 88
+  val h_sync = 128
+  val h_display = 800
 
-  val line_width = h_display + h_sync + h_back_porch + h_front_porch
+  val frame_width = h_display + h_front_porch + h_sync + h_back_porch
 
-  val h_cntReg = RegInit(0.U(1.W))
-  val pixel_clock = RegInit(0.U(1.W))
+  val h_cntReg = RegInit(0.U(log2Ceil(frame_width).W))
 
+  // Generate Counter
   h_cntReg := h_cntReg + 1.U
-  when(h_cntReg === line_width) {
+  when(h_cntReg === frame_width.U) {
     h_cntReg := 0.U
+    v_cntReg := v_cntReg + 1.U
   }
 
-  /* Vertical */
-  val v_front_porch = 1.U // in lines
-  val v_back_porch = 23.U
-  val v_sync = 4.U
+  when(v_cntReg === frame_height.U) {
+    v_cntReg := 0.U
+  }
+
+  // output v_sync
+  when (v_cntReg > (v_display + v_front_porch).U && v_cntReg < (v_display + v_front_porch + v_sync).U) {
+    io.v_sync := 1.U
+  }
+  .otherwise {
+    io.v_sync := 0.U
+  }
+
+  // output h_sync
+  when (h_cntReg > (h_display + h_front_porch).U && h_cntReg < (h_display + h_front_porch + h_sync).U) {
+    io.h_sync := 1.U
+  }
+  .otherwise {
+    io.h_sync := 0.U
+  }
+
+  // output n_blank
+  when (h_cntReg < h_display.U && v_cntReg < v_display.U) {
+    io.n_blank := 1.U
+  }
+  .otherwise {
+    io.n_blank := 0.U
+  }
 }
--- src/test/scala/simple/VGADriverTest.scala
+++ src/test/scala/simple/VGADriverTest.scala
@@ -9,13 +9,12 @@ import VGADriver._
  * Test the VGADriver design
  */
 class VGADriverTester(dut: VGADriver) extends PeekPokeTester(dut) {
-        step(500)
+  step(5000000)
 }
 
 
 object VGADriverTest extends App {
   iotesters.Driver.execute(Array("--target-dir", "generated", "--generate-vcd-output", "on"), () => new VGADriver()) {
-  // iotesters.Driver.execute(Array("--target-dir", "generated", "--fint-write-vcd", "--wave-form-file-name", "generated/BubbleFifo.vcd"), () => new BubbleFifo(8, 4)) {
     c => new VGADriverTester(c)
   }
 }
